dfe3a9950bb1fff3461c83849cbcdf79ad37e982
Sion Williams <sion@byteshifter.io>
2018-02-14 15:27:39 +0000

    Check service and display
    Hit each of the endpoints and get a status then display in the template.

--- main.go
+++ main.go
@@ -17,10 +17,26 @@ func init() {
 
 // Config holds a list of services to be
 // checked
-type Config []struct {
-	Type string `json:"type"`
-	URL  string `json:"url"`
-	Port string `json:"port"`
+type Config struct {
+	Services []status.Service `json:"services"`
+}
+
+// CreateFactories will return a slice of Pinger concrete services
+func (c *Config) CreateFactories() []status.Pinger {
+	var checks []status.Pinger
+
+	for _, service := range c.Services {
+		switch service.Type {
+		case "ping":
+			p := status.PingFactory{}
+			checks = append(checks, p.Create(service))
+		case "grep":
+			g := status.GrepFactory{}
+			checks = append(checks, g.Create(service))
+		}
+	}
+
+	return checks
 }
 
 // LoadConfiguration takes a configuration file and returns
@@ -47,19 +63,25 @@ func main() {
 	fmt.Println("Starting the application...")
 	// read the config file to determine which services need to be checked
 	config, _ := LoadConfiguration(configPath)
-	// :debug
-	fmt.Println(config)
+
+	services := config.CreateFactories()
+
+	down := make(map[string]int)
+	var up []string
+
+	for _, service := range services {
+		err := service.Status()
+		if err != nil {
+			down[service.GetService().URL] = 60
+			break
+		}
+		up = append(up, service.GetService().URL)
+	}
 
 	statushtml := template.HTML(`<div class="alert alert-success" role="alert">
 	<span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
 	All Systems Operational
 </div>`)
-
-	up := []string{"ping google.com"}
-	down := map[string]int{
-		"ping googlex.com":    60,
-		"grep heisenberg.net": 30,
-	}
 
 	p := status.Page{
 		Title:  "My Status",
--- status/check.go
+++ status/check.go
@@ -13,16 +13,34 @@ var (
 	ErrRegexNotFound      = errors.New("commands: regex not found")
 )
 
+// Service represents a single endpoint to be tested
+type Service struct {
+	Type  string `json:"type"`
+	URL   string `json:"url"`
+	Port  string `json:"port,omitempty"`
+	Regex string `json:"regex,omitempty"`
+}
+
 // Pinger is an interface for all modes of testing
 // a services availability
 type Pinger interface {
+	GetService() *Service
 	Status() error
+}
+
+type PingerFactory interface {
+	Create(Service) Pinger
 }
 
 // Ping performs a ping-like test of a
 // services availability
 type Ping struct {
-	URL string
+	Service
+}
+
+// GetService return the Service pointer
+func (p *Ping) GetService() *Service {
+	return &p.Service
 }
 
 // Status sends a HEAD http request and checks for a valid
@@ -40,10 +58,22 @@ func (p *Ping) Status() error {
 	return nil
 }
 
+type PingFactory struct{}
+
+func (factory *PingFactory) Create(s Service) Pinger {
+	return &Ping{
+		Service: Service{URL: s.URL},
+	}
+}
+
 // Grep checks a response body for a value
 type Grep struct {
-	URL   string
-	Regex string
+	Service
+}
+
+// GetService return the Service pointer
+func (p *Grep) GetService() *Service {
+	return &p.Service
 }
 
 // Status requests a page given a URL and checks the response for
@@ -67,6 +97,14 @@ func (p *Grep) Status() error {
 	}
 
 	return nil
+}
+
+type GrepFactory struct{}
+
+func (factory *GrepFactory) Create(s Service) Pinger {
+	return &Grep{
+		Service: Service{URL: s.URL, Regex: s.Regex},
+	}
 }
 
 // validStatus checks the input against a list of known-good
